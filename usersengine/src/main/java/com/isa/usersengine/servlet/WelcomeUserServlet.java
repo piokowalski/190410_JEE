2edaa31914864c63d178b039d9f969c1135c7c55
rafalmisiak <rafalmisiak@gmail.com>
2019-04-17 13:31:28 +0200

    fm config

--- usersengine/src/main/java/com/isa/usersengine/servlet/WelcomeUserServlet.java
+++ usersengine/src/main/java/com/isa/usersengine/servlet/WelcomeUserServlet.java
@@ -1,14 +1,27 @@
 package com.isa.usersengine.servlet;
 
+import com.isa.usersengine.freemarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import javax.inject.Inject;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
 
 @WebServlet("/welcome-user")
 public class WelcomeUserServlet extends HttpServlet {
+
+    private static final Logger logger = Logger.getLogger(WelcomeUserServlet.class.getName());
+
+    @Inject
+    TemplateProvider templateProvider;
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -19,10 +32,16 @@ public class WelcomeUserServlet extends HttpServlet {
             return;
         }
 
-        PrintWriter writer = resp.getWriter();
+        Template template = templateProvider.getTemplate(getServletContext(), "welcome-user.ftlh");
+        Map<String, Object> model = new HashMap<>();
 
-        writer.println("<!DOCTYPE html><html><body>");
-        writer.println("Hello " + name + "!");
-        writer.println("</body></html>\n");
+        model.put("name", name);
+
+        try {
+            template.process(model, resp.getWriter());
+        } catch (TemplateException e) {
+            logger.severe(e.getMessage());
+        }
+
     }
 }
